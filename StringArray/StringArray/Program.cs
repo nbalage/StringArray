b3b49a42e3ced8699618aa80439a3fa58a33f00f
Balazs Nemeth <Balazs_Nemeth@epam.com>
2019-04-30 15:13:18 +0200

    Everything other

--- StringArray/StringArray/Program.cs
+++ StringArray/StringArray/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace StringArray
 {
@@ -19,9 +18,43 @@ namespace StringArray
                 "Now we are the kids from yesterday"
             };
 
+            Console.WriteLine("1.");
             foreach (string s in strings)
             {
                 Console.WriteLine($"\"{s}\" has {WordCount(s)} words.");
+            }
+
+            Console.WriteLine("2.");
+            foreach (string s in strings)
+            {
+                foreach (var word in StartWithVowel(s))
+                {
+                    Console.Write($"{word} ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("3.");
+            foreach (string s in strings)
+            {
+                Console.WriteLine($"{LongestWord(s)}");
+            }
+
+            Console.WriteLine("4.");
+            foreach (string s in strings)
+            {
+                Console.WriteLine($"Average word length: {AverageWordCount(s)}.");
+            }
+
+            Console.WriteLine("5.");
+            foreach (string s in strings)
+            {
+                var orderedWords = WordsInAlphabeticOrder(s);
+                foreach (var word in orderedWords)
+                {
+                    Console.Write($"{word} ");
+                }
+                Console.WriteLine();
             }
 
             Console.ReadKey();
@@ -32,6 +65,50 @@ namespace StringArray
             var count = sentence.Count(c => c == ' ');
 
             return count + 1;
+        }
+
+        private static IEnumerable<string> StartWithVowel(string sentence)
+        {
+            var chars = new List<char>
+            {
+                'a', 'e', 'i', 'o', 'u'
+            };
+            var splits = sentence.Split(' ');
+
+            return splits.Where(c => chars.Contains(c.ToLower().First()));
+        }
+
+        private static string LongestWord(string sentence)
+        {
+            var lengths = WordsWithLenght(sentence);
+
+            return lengths.OrderByDescending(l => l.count).Select(l => l.word).First();
+        }
+
+        private static double AverageWordCount(string sentence)
+        {
+            var lengths = WordsWithLenght(sentence);
+
+            return lengths.Average(l => l.count);
+        }
+
+        private static IOrderedEnumerable<string> WordsInAlphabeticOrder(string sentence)
+        {
+            var splits = sentence.Split(' ');
+
+            return splits.Distinct().OrderBy(s => s[0]);
+        }
+
+        private static List<(string word, int count)> WordsWithLenght(string sentence)
+        {
+            List<(string word, int count)> lengths = new List<(string word, int count)>();
+
+            foreach (var item in sentence.Split(' '))
+            {
+                lengths.Add((item, item.Length));
+            }
+
+            return lengths;
         }
     }
 }

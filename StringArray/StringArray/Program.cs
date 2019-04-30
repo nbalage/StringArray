b44a8d133bfa221cdd5f7852a09c41cd5249be00
Balazs Nemeth <Balazs_Nemeth@epam.com>
2019-04-30 14:39:45 +0200

    Word Count

---
+++ StringArray/StringArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace StringArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings =
+            {
+                "You only live forever in the lights you make",
+                "When we were young we used to say",
+                "That you only hear the music when your heart begins to break",
+                "Now we are the kids from yesterday"
+            };
+
+            foreach (string s in strings)
+            {
+                Console.WriteLine($"\"{s}\" has {WordCount(s)} words.");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int WordCount(string sentence)
+        {
+            var count = sentence.Count(c => c == ' ');
+
+            return count + 1;
+        }
+    }
+}
